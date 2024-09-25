172081ac5365ad70347780df898134d5e2def69c
Parabellum12 <amessmer@themessmers.net>
2024-09-25 21:38:40 +0000

    example program 341

---
+++ css342/lecture1/exampleCppProject/hello_world.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name;
+    name = (string) "Jimmy" + (string) " Hoffa";
+    cout << name << endl;
+    string tmp;
+    cout << "Enter:";
+    cin >> tmp;
+    cout << tmp << endl;
+}
