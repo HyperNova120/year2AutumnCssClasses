2507ddebcb59325518f411a32d3b94f2504d2c76
Parabellum12 <amessmer@themessmers.net>
2024-10-10 03:40:11 +0000

    push

--- css342/program1/time_span.cpp
+++ css342/program1/time_span.cpp
@@ -1,5 +1,6 @@
 #include "time_span.h"
 
+// constructors
 TimeSpan::TimeSpan()
 {
     hours_ = 0;
@@ -18,51 +19,42 @@ TimeSpan::TimeSpan(double hoursin, double minutesin, double secondsin) : TimeSpa
 
 void TimeSpan::set_time(int hoursin, int minutesin, int secondsin)
 {
-    SetFromTotalSeconds((hoursin*3600)+(minutesin*60)+secondsin);
+    SetFromTotalSeconds((hoursin * 3600) + (minutesin * 60) + secondsin);
 }
 
-void TimeSpan::SetFromTotalSeconds(double totalSeconds)
-{
-    int secIn = round(totalSeconds);
-    hours_ = secIn / 3600;
-    secIn = secIn % 3600;
-    minutes_ = secIn / 60;
-    seconds_ = secIn % 60;
-}
-
+// getters
 int TimeSpan::hours() const
 {
     return hours_;
 }
+
 int TimeSpan::minutes() const
 {
     return minutes_;
 }
+
 int TimeSpan::seconds() const
 {
     return seconds_;
 }
 
-int TimeSpan::TotalSeconds() const
-{
-    return seconds_ + (minutes_ * 60) + (hours_ * 3600);
-}
-
+// i/ostream operator overloads
 ostream &operator<<(ostream &os, const TimeSpan &obj)
 {
     os << "Hours: " << obj.hours() << ", Minutes: " << obj.minutes() << ", Seconds: " << obj.seconds();
     return os;
 }
 
-//takes istream input as int int int 
+// takes istream input as int int int
 istream &operator>>(istream &is, TimeSpan &obj)
 {
     int hrs, mins, secs;
     is >> hrs >> mins >> secs;
-    obj.SetFromTotalSeconds((hrs*3600) + (mins * 60) + secs);
+    obj.SetFromTotalSeconds((hrs * 3600) + (mins * 60) + secs);
     return is;
 }
 
+// operator overloads
 TimeSpan TimeSpan::operator+(const TimeSpan &obj) const
 {
     return TimeSpan(TotalSeconds() + obj.TotalSeconds());
@@ -131,3 +123,21 @@ bool TimeSpan::operator>(const TimeSpan &obj) const
 {
     return TotalSeconds() > obj.TotalSeconds();
 };
+
+// private functions
+
+// converts a number of seconds to appropriate # of hrs and mins using
+// conversion ratios of 3600sec:1hr and 60sec:1min
+void TimeSpan::SetFromTotalSeconds(double totalSeconds)
+{
+    int secIn = round(totalSeconds);
+    hours_ = secIn / 3600;
+    secIn = secIn % 3600;
+    minutes_ = secIn / 60;
+    seconds_ = secIn % 60;
+}
+
+int TimeSpan::TotalSeconds() const
+{
+    return seconds_ + (minutes_ * 60) + (hours_ * 3600);
+}
