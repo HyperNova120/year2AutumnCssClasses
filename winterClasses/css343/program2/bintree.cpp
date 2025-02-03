c33e0f21b3f462a8e6afa133c7daf439c92279b2
Aidan Messmer <amessmer@themessmers.net>
2025-02-03 02:52:02 +0000

    auto push

--- winterClasses/css343/program2/bintree.cpp
+++ winterClasses/css343/program2/bintree.cpp
@@ -1,28 +1,38 @@
 #include "bintree.h"
 
+/// @brief defualt constructor
 BinTree::BinTree() {}
 
+/// @brief copy constructor
+/// @param other BinTree to copy
 BinTree::BinTree(const BinTree &other)
 {
     *this = other;
 }
 
+/// @brief deconstructor
 BinTree::~BinTree()
 {
     deleteHelper(root);
     root = nullptr;
 }
 
+/// @brief returns if BinTree is empty
+/// @return true if BinTree is empty, false if not
 bool BinTree::isEmpty() const
 {
     return root == nullptr;
 }
 
+/// @brief calls deconstructor to empty BinTree
 void BinTree::makeEmpty()
 {
     this->~BinTree();
 }
 
+/// @brief assignment operator
+/// @param other BinTree to perform deep copy of
+/// @return BinTree for chaining
 BinTree &BinTree::operator=(const BinTree &other)
 {
     if (this == &other)
@@ -46,6 +56,9 @@ BinTree &BinTree::operator=(const BinTree &other)
     return *this;
 }
 
+/// @brief equality operator
+/// @param other BinTree to test equality of
+/// @return true if BinTree is equal to other, false if not
 bool BinTree::operator==(const BinTree &other) const
 {
     if (this == &other)
@@ -55,11 +68,17 @@ bool BinTree::operator==(const BinTree &other) const
     return equivalencyHelper(root, other.root);
 }
 
+/// @brief non-equality operator
+/// @param other BinTree to test non-equality of
+/// @return true if BinTree is not equal to other, false if not
 bool BinTree::operator!=(const BinTree &other) const
 {
     return !(*this == other);
 }
 
+/// @brief inserts a deep copy of data into BinTree if no duplicate exists
+/// @param data object to insert into BinTree
+/// @return true if insertion was successful, false if a duplicate exist
 bool BinTree::insert(NodeData *data)
 {
     data = new NodeData(*data);
@@ -90,19 +109,25 @@ bool BinTree::insert(NodeData *data)
     return true;
 }
 
+/// @brief returns a pointer to the object within the BinTree if found
+/// @param target NodeData to find
+/// @param nodeData pointer to found object. gets set to nullptr if not found
+/// @return true if target was found, false if not
 bool BinTree::retrieve(const NodeData &target, NodeData *&nodeData) const
 {
     Node *targetNode = getNode(target);
 
     if (targetNode != nullptr && *targetNode->data == target)
     {
-        nodeData = new NodeData(*targetNode->data);
+        nodeData = targetNode->data;
         return true;
     }
     nodeData = nullptr;
     return false;
 }
 
+/// @brief recursively deletes starting from curNode
+/// @param curNode root of (sub)-tree to delete
 void BinTree::deleteHelper(Node *curNode)
 {
     if (curNode == nullptr)
@@ -116,6 +141,10 @@ void BinTree::deleteHelper(Node *curNode)
     delete curNode->data;
 }
 
+/// @brief recursive helper to test equality
+/// @param curNode BinTree node to test
+/// @param otherCurNode other BinTree node to test
+/// @return true if curNode == otherCurNode, false otherwise
 bool BinTree::equivalencyHelper(const Node *curNode, const Node *otherCurNode) const
 {
     if ((curNode == nullptr) ^ (otherCurNode == nullptr))
@@ -134,6 +163,9 @@ bool BinTree::equivalencyHelper(const Node *curNode, const Node *otherCurNode) c
            equivalencyHelper(curNode->right, otherCurNode->right);
 }
 
+/// @brief gets the height of curNode recursively
+/// @param curNode node to get the height of
+/// @return height of curNode
 int BinTree::getHeightHelper(const Node *curNode) const
 {
     if (curNode == nullptr)
@@ -152,6 +184,9 @@ int BinTree::getHeightHelper(const Node *curNode) const
     return 1 + height;
 }
 
+/// @brief returns a node pointer to the node containing target
+/// @param target nodeData to search for
+/// @return pointer to node containing target
 BinTree::Node *BinTree::getNode(const NodeData &target) const
 {
     Node *reader = root;
@@ -168,6 +203,9 @@ BinTree::Node *BinTree::getNode(const NodeData &target) const
     return nullptr;
 }
 
+/// @brief gets the height of the node containing target
+/// @param target nodeData to search for
+/// @return height of node containing target
 int BinTree::getHeight(const NodeData &target) const
 {
     Node *targetNode = getNode(target);
@@ -212,6 +250,8 @@ void BinTree::sideways(Node *current, int level) const
     }
 }
 
+/// @brief empties BinTree into array as in-order NodeData
+/// @param arr nodeData ponter array to populate with BinTree nodes
 void BinTree::bstreeToArray(NodeData *arr[])
 {
     for (int i = 0; i < K_ARRAY_MAX_SIZE; i++)
@@ -227,6 +267,10 @@ void BinTree::bstreeToArray(NodeData *arr[])
     this->~BinTree();
 }
 
+/// @brief recursively adds nodes in-order to arr
+/// @param curNode current node to add to arr
+/// @param arr array to populate with BinTree
+/// @param index arr index to set next NodeData to
 void BinTree::bstreeToArrayHelper(Node *curNode, NodeData *arr[], int &index)
 {
     if (curNode == nullptr)
@@ -239,6 +283,8 @@ void BinTree::bstreeToArrayHelper(Node *curNode, NodeData *arr[], int &index)
     bstreeToArrayHelper(curNode->right, arr, index);
 }
 
+/// @brief empties arr to create a balanced BinTree
+/// @param arr NodeData array to remove and populate BinTree with
 void BinTree::arrayToBSTree(NodeData *arr[])
 {
     this->~BinTree();
@@ -261,6 +307,10 @@ void BinTree::arrayToBSTree(NodeData *arr[])
     }
 }
 
+/// @brief recursively insert NodeData from arr into BinTree
+/// @param arr NodeData array to populate BinTree with
+/// @param low min arr index to populate from
+/// @param high max arr index to populate from
 void BinTree::arrayToBSTreeHelper(NodeData *arr[], int low, int high)
 {
     if (high < low)
@@ -284,6 +334,10 @@ void BinTree::arrayToBSTreeHelper(NodeData *arr[], int low, int high)
     arrayToBSTreeHelper(arr, mid + 1, high);
 }
 
+/// @brief ostream operator
+/// @param os ostream object to output to
+/// @param obj BinTree to print
+/// @return ostream with obj printed to
 ostream &operator<<(ostream &os, BinTree obj)
 {
     obj.inorderHelper(obj.root, os);
@@ -291,6 +345,9 @@ ostream &operator<<(ostream &os, BinTree obj)
     return os;
 }
 
+/// @brief recursively output curNode in-order to os
+/// @param curNode root of (sub)-tree to print out in-order
+/// @param os ostream to print to
 void BinTree::inorderHelper(Node *curNode, ostream &os) const
 {
     if (curNode == nullptr)
